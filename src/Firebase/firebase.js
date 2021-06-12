f6c618928057513d7df070eb3d228bf62b2e5108
selmaBBK <selma.boubakri@hotmail.com>
2021-06-12 20:54:49 +0200

    added and config Firebase

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,25 @@
+import app from 'firebase/app';
+
+/**
+ * Firebase configuration
+ */
+const config = {
+    apiKey: "AIzaSyB_rObsT9i0qM8dkhmnOLiRc0ZY00ma9bc",
+    authDomain: "my-test-geekeur.firebaseapp.com",
+    projectId: "my-test-geekeur",
+    storageBucket: "my-test-geekeur.appspot.com",
+    messagingSenderId: "279794824954",
+    appId: "1:279794824954:web:a0aecfd787c23804443b43"
+};
+
+/**
+* Initialize Firebase
+ */
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config)
+    }
+}
+
+export default Firebase;
